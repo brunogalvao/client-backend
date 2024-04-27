9fab4a489c58a6a8f75894c1d23be6cd86e11ccc
Bruno Galvao <bruno_galvao@outlook.com>
2024-04-27 08:57:40 -0300

    send files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto API Cadastro de Cliente
+
+Projeto de api para cadastro de cliente usando 'fastify', 'primas'  e 'express'.
+
+
---
+++ src/routes.ts
@@ -0,0 +1,34 @@
+import {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
+import { ListCustomerController } from "./controllers/ListCustomerController";
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController";
+
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions,
+) {
+  fastify.get(
+    "/teste",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return { hello: "world" };
+    },
+  );
+
+  fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+    return new CreateCustomerController().handle(request, reply);
+  });
+
+  fastify.get("/customers", async (request: FastifyRequest, reply: FastifyReply) => {
+    return new ListCustomerController().handle(request, reply);
+  });
+
+  fastify.delete("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+    return new DeleteCustomerController().handle(request, reply);
+  });
+
+}
